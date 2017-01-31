546a760170f30c0a7d0c17644f160e922b431f1e
Dan Bohea <git@bohea.co.uk>
2017-01-31 17:43:04 +0000

    Initial commit

---
+++ templates/settings.local.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * A settings.local.php file for Drupal.
+ */
+
+// Add your database configuration here.
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'host' => '127.0.0.1',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'database' => 'drupal',
+  'prefix' => '',
+);
